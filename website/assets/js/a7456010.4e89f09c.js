eb34ce3b3528611462031ffd52773cef7f3313b8
Thomas Tang <hello@tangkunyin.com>
2025-01-16 14:19:30 +0800

    change default build dir
    Signed-off-by: Thomas Tang <hello@tangkunyin.com>

---
+++ website/assets/js/a7456010.4e89f09c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpynotes=self.webpackChunkpynotes||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ website/assets/js/aba21aa0.2884f608.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpynotes=self.webpackChunkpynotes||[]).push([[742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
